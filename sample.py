fafb0c07f0f4d434473415c0bfe83ed09413684a
ymtm123 <yamazaki@kanagawa-u.ac.jp>
2021-11-30 09:59:06 +0900

    wordのコメントアウト

--- sample.py
+++ sample.py
@@ -3,7 +3,7 @@ import text_mining
 target_dir = './text_report'
 
 text_mining.show_conetwork(text_dir=target_dir,
-                           word="ファッション",
+                           # word="ファッション",
                            edge_threshold=2,
                            k=0.1,
                            sw_list=['こと', 'とき', 'もの', 'ところ', 'いう', 'お', 'しれ'])
